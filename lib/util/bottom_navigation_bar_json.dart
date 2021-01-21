0d91c3bf0e088a67c486e76a73858e189af75dc6
sathya <sathya@bxpress.delivery>
2021-01-21 22:47:25 +0700

    add util data

---
+++ lib/util/bottom_navigation_bar_json.dart
@@ -0,0 +1,23 @@
+
+const icons = [
+  {
+    "inactive": "assets/images/home.svg",
+    "active": "assets/images/home_active.svg",
+  },
+  {
+    "inactive": "assets/images/search.svg",
+    "active": "assets/images/search_active.svg",
+  },
+  {
+    "inactive": "assets/images/add.svg",
+    "active": "assets/images/add_active.svg",
+  },
+  {
+    "inactive": "assets/images/heart.svg",
+    "active": "assets/images/heart_active.svg",
+  },
+  {
+    "inactive": "assets/images/account.svg",
+    "active": "assets/images/account_active.svg",
+  },
+];
